76949c18341bc87d928362693eb4b013bd6aa0f1
hasanmir <mohasan1384@gmail.com>
2024-01-30 11:04:15 +0330

    create Equal function and finish

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ let buttons = document.querySelectorAll('button')
 for(let button of buttons){
     button.addEventListener('click',()=>{
         if(button.innerHTML != 'AC' && button.innerHTML != 'DE'){
-        display.value += button.innerHTML;
+            display.value += button.innerHTML;
+            console.log(display.value)
+            if(button.innerHTML == '='){
+                Equal(display.value = display.value.replace('=',''));
+            }
         }
         else{
             if(button.innerHTML == 'AC'){
@@ -26,4 +30,13 @@ function clearAll(){
 
 function clearOne(){
     display.value = display.value.replace(display.value[display.value.length-1] , '')
+}
+
+function Equal(equation){
+    try{
+        display.value = eval(equation)
+    }
+    catch(error){
+        display.value = 'ERROR'
+    }
 }
