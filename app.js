c7f6591e886515eb72e52e484da8448145797318
hasanmir <mohasan1384@gmail.com>
2024-01-30 10:52:35 +0330

    create function for clear all and clear one number

--- app.js
+++ app.js
@@ -7,6 +7,23 @@ for(let button of buttons){
     button.addEventListener('click',()=>{
         if(button.innerHTML != 'AC' && button.innerHTML != 'DE'){
         display.value += button.innerHTML;
-    }
-})
+        }
+        else{
+            if(button.innerHTML == 'AC'){
+                clearAll()
+            }
+            else{
+                clearOne()
+            }
+        }
+    })
+}
+
+
+function clearAll(){
+    display.value = ''
+}
+
+function clearOne(){
+    display.value = display.value.replace(display.value[display.value.length-1] , '')
 }
